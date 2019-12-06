7de2af1912d6fb80813d3a9ef096498dba99a93f
Dustin Martinez <martinezdustin736@gmail.com>
2019-12-06 02:34:35 -0600

    liri files

---
+++ liri.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var axios = require("axios");
+
+var command = process.argv[2];
+console.log(command);
+var value = process.argv[3];
+console.log(value);
+
+switch(command) {
+    case 'movie-this':
+    movie();
+    break; 
+}
+
+function movie(value) {
+    if(!value){
+        value = "mr nobody";
+    }
+    axios.get("https://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy")
+    .then(function(response) {
+            var movieResults = 
+                "--------------------------------------------------------------------" +
+                    "\nMovie Title: " + response.data.Title + 
+                    "\nYear of Release: " + response.data.Year +
+                    "\nIMDB Rating: " + response.data.imdbRating +
+                    "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                    "\nCountry Produced: " + response.data.Country +
+                    "\nLanguage: " + response.data.Language +
+                    "\nPlot: " + response.data.Plot +
+                    "\nActors/Actresses: " + response.data.Actors;
+            console.log(movieResults);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    
+}
